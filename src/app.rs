96d07d82e4737a2c2fb132fd4a3cb54e8e42de3b
Fernando Vega <pytness@gmail.com>
2026-05-21 23:44:53 +0200

    feat: read and redraw on kpres

--- src/app.rs
+++ src/app.rs
@@ -177,7 +177,11 @@ impl<'a> App<'a> {
         }
 
         self.term.ttywrite(&buffer, len, true);
-        // self.term.ttyread();
+        self.term.ttyread();
+
+        if let Some(window) = self.app_state.as_ref().map(|s| &s.window) {
+            window.request_redraw();
+        }
     }
     pub fn cmessage(&mut self) {}
 
