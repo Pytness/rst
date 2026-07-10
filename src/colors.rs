e5f59435ae1931aa141a20a0f4b08a61150d9424
Fernando Vega <pytness@gmail.com>
2026-07-10 09:13:45 +0200

    feat: partial implementation of colors

--- src/colors.rs
+++ src/colors.rs
@@ -1,3 +1,6 @@
+use crate::BETWEEN;
+use crate::config::DEFAULTBG;
+
 pub const COLORS: [u32; 512] = {
     let mut arr = [0; 512];
     // 8 normal colors
@@ -28,3 +31,116 @@ pub const COLORS: [u32; 512] = {
 
     arr
 };
+
+#[derive(Default, Clone)]
+pub struct Color {
+    pub red: u16,
+    pub green: u16,
+    pub blue: u16,
+    pub alpha: u16,
+}
+
+pub struct ColorRegistry {
+    loaded: bool,
+    colors: Vec<Color>,
+}
+
+impl ColorRegistry {
+    pub fn load_colors(&mut self) {
+        if self.loaded {
+            for color in self.colors.iter() {
+                // XftColorFree(xw.dpy, xw.vis, xw.cmap, cp);
+            }
+        } else {
+            let len = COLORS.len().max(256);
+            self.colors = vec![Color::default(); len];
+        }
+
+        for i in 0..self.colors.len() {
+            if let Some(color) = self.load_color(i, None) {
+                self.colors[i] = color;
+            }
+        }
+
+        // TODO: check if this is needed. Maybe opengl makes this obsolete
+
+        // dc.col[defaultbg].color.alpha = (unsigned short)(0xffff * alpha);
+        // dc.col[defaultbg].pixel &= 0x00FFFFFF;
+        // dc.col[defaultbg].pixel |= (unsigned char)(0xff * alpha) << 24;
+
+        self.loaded = true;
+    }
+
+    pub fn load_color(&mut self, i: usize, name: Option<&str>) -> Option<Color> {
+        let mut name = name;
+        let mut color = Color::default();
+
+        if let Some(name) = name {
+            todo!("Implement color name parsing for '{}'", name);
+        }
+
+        const XTERM_SIZE: u16 = 6 * 6 * 6 + 16;
+
+        if name.is_none() {
+            if BETWEEN!(i, 16, 255) {
+                // 256 color
+                if i < XTERM_SIZE as usize {
+                    // same colors as xterm
+                    color.red = sixd_to_16bit(((i - 16) / 36) % 6);
+                    color.green = sixd_to_16bit(((i - 16) / 6) % 6);
+                    color.blue = sixd_to_16bit(((i - 16) / 1) % 6);
+                } else {
+                    // TODO: un-magic this
+                    color.red = 0x0808 + 0x0a0a * (i as u16 - XTERM_SIZE);
+                    color.green = color.red;
+                    color.blue = color.red;
+                }
+
+                return Some(color);
+            } else {
+                // TODO: parse the color from `name`
+                // c code: name = colorname[i];
+                name = Some(COLORS[i].to_string().as_str());
+            }
+        }
+
+        // TODO: implement color parsing from name
+
+        return Some(color);
+    }
+
+    pub fn get_color(&self, i: usize) -> Option<&Color> {
+        self.colors.get(i)
+    }
+
+    pub fn set_color_name(&mut self, i: usize, name: &str) -> bool {
+        if i >= self.colors.len() {
+            return false;
+        }
+
+        if let Some(color) = self.load_color(i, Some(name)) {
+            self.colors[i] = color;
+
+            if i == DEFAULTBG as usize {
+                // NOTE: this originaly was multiplied by `alpha`, a configurable constant
+                // TODO: check if this is needed. Maybe opengl makes this obsolete
+                // dc.col[defaultbg].color.alpha = (unsigned short)(0xffff * alpha);
+                // dc.col[defaultbg].pixel &= 0x00FFFFFF;
+                // dc.col[defaultbg].pixel |= (unsigned char)(0xff * alpha) << 24;
+            }
+        } else {
+            return false;
+        }
+
+        return true;
+    }
+}
+
+// TODO: un-magic this
+fn sixd_to_16bit(x: usize) -> u16 {
+    if x == 0 {
+        0
+    } else {
+        0x3737 + 0x2828 * x as u16
+    }
+}
