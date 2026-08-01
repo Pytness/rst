63264207fdc376c192ad98862c3e9b7e64a7fa47
Fernando Vega <pytness@gmail.com>
2026-08-02 01:53:36 +0200

    fix: tscrollup panics when scroll count exceeds region height

--- src/term_state.rs
+++ src/term_state.rs
@@ -372,12 +372,10 @@ impl TermState {
         self.tclearregion(0, orig, self.col - 1, orig + n - 1);
         self.tsetdirt(orig + n, self.bot);
 
-        for i in orig..=(self.bot.saturating_sub(n)) {
-            // let temp = self.line[i].to_owned();
-            // self.line[i] = self.line[i + n];
-            // self.line[i + n] = temp;
-
-            self.line.swap(i, i + n);
+        if let Some(range) = self.bot.checked_sub(n) {
+            for i in orig..=range {
+                self.line.swap(i, i + n);
+            }
         }
 
         /* move images, if they are inside the scrolling region */
