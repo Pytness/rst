8cf0b8bfd3e0924ec15a463bd77d121f1e080e9c
Fernando Vega <pytness@gmail.com>
2026-08-01 21:56:40 +0200

    fix: missing NUL terminator in snprintf format strings

--- src/csiesq.rs
+++ src/csiesq.rs
@@ -453,13 +453,18 @@ impl CSIEscape {
 
                     // Report character cell sie in pixels
                     16 => {
-                        let len = snprintf!(buffer, "\x1b[6;%i;%it", state.pixh / state.row, state.pixw / state.col);
+                        let len = snprintf!(
+                            buffer,
+                            b"\x1b[6;%i;%it\0",
+                            state.pixh / state.row,
+                            state.pixw / state.col
+                        );
                         state.ttywrite_pty(&buffer, len as usize);
                     }
 
                     // Report the size of the text area in characters
                     18 => {
-                        let len = snprintf!(buffer, "\x1b[8;%i;%it", state.row, state.col);
+                        let len = snprintf!(buffer, b"\x1b[8;%i;%it\0", state.row, state.col);
                         state.ttywrite_pty(&buffer, len as usize);
                     }
 
@@ -485,7 +490,8 @@ impl CSIEscape {
                         };
 
                         let mut buffer = [0u8; 40];
-                        let len = snprintf!(buffer, "\x1b[?%d;%d$y", self.arg[0], feature_mode);
+                        let len =
+                            snprintf!(buffer, b"\x1b[?%d;%d$y\0", self.arg[0], feature_mode);
                         state.ttywrite_pty(&buffer, len as usize);
                     }
                     _ => unknown(),
